9f68b487dbfcf3409ed5b90e91dfed26d172e075
Ofrepose <paynedanielfranklin@gmail.com>
2020-09-08 23:17:45 -0400

    Setup store-actions-reducers-formHooks

---
+++ src/components/forms/businessSignInForm.js
@@ -0,0 +1,105 @@
+import React,{useState, useEffect} from 'react';
+import TextField from "./textField";
+import PasswordField from './passwordField';
+import * as yup from "yup";
+
+
+function BusinessSignInForm(props){
+
+    const defaultState = {
+
+        email: "",
+        password: "",
+
+    }
+
+    const [formState, setFormState] = useState(defaultState);
+    const [errors, setErrors] = useState({...defaultState})
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+
+
+
+    let formSchema = yup.object().shape({
+
+        email: yup.string().email("This is not a valid email").required("Please enter your email."),
+        password: yup.string().required("Password is required").min(6, "Passwords must be at least 6 characters in length"),
+
+    });
+
+    //Turns button off unless form is correctly filled
+    useEffect(() => {
+
+        formSchema.isValid(formState).then(valid => {
+            setButtonDisabled(!valid)
+        });
+    }, [formState]);
+
+
+    //Entering and submitting data
+    const submitData = e => {
+        e.preventDefault();
+        //Axios post here
+        props.register(formState)
+    }
+
+    const handleChange = e => {
+
+        setFormState({
+            ...formState, [e.target.name]: e.target.value
+        });
+
+        inputChange(e);
+    }
+
+    const inputChange = e => {
+        e.persist();
+        yup
+            .reach(formSchema, e.target.name)
+            .validate(e.target.value)
+            .then(valid => {
+                setErrors({
+                    ...errors, [e.target.name]: ""
+                });
+            })
+            .catch(err => {
+                setErrors({...errors, [e.target.name]: err.errors[0]})
+            });
+    };
+
+    return(
+        <>
+            <form className="businessSignUp">
+
+                <TextField label = 'Email Address'
+                           name="email"
+                           value={formState.email}
+                           handleChange={handleChange}
+                />
+
+                <br />
+
+                <PasswordField label='Password'
+                               name="password"
+                               value={formState.password}
+                               handleChange={handleChange}
+                />
+
+                <br />
+
+                <div className="underFormText">New Business?<span className="blueTextDefault" onClick={()=>{props.stateChanger('intro')}}> Create Account</span></div>
+                <br />
+
+                <div className="f_dataS" disabled={buttonDisabled}>Submit</div>
+
+            </form>
+
+            <div className="errorDiv">
+                {errors.email.length > 0 ? (<p className="error">{errors.email}</p>):
+                    errors.password.length > 0 ? (<p className="error">{errors.password}</p>): null}
+
+            </div>
+        </>
+    )
+}
+
+export default BusinessSignInForm;
---
+++ src/utils/actions/actionsIndex.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { axiosWithAuth } from '../axiosWithAuth';
+import { useHistory } from 'react-router-dom';
+
+export const INITIAL_LOAD_START = 'INITIAL_LOAD_START'
+export const INITIAL_LOAD_SUCCESS = 'INITIAL_LOAD_SUCCESS'
+export const INITIAL_LOAD_FAILURE = 'INITIAL_LOAD_FAILURE'
+
+//login Developer
+export const LOGIN_DEVELOPER_START = 'LOGIN_DEVELOPER_START'
+export const LOGIN_DEVELOPER_SUCCESS = 'LOGIN_DEVELOPER_SUCCESS'
+export const LOGIN_DEVELOPER_FAILURE = 'LOGIN_DEVELOPER_FAILURE'
+
+const apiURL = 'srsrtser'
+const apiURL_Login_Developer = 'asdfasfda'
+
+export const initialLoad = () => dispatch =>{
+    console.log('inside initial load action')
+
+        dispatch({ type: INITIAL_LOAD_START })
+        axios.get(apiURL)
+            .then(res => {
+                dispatch({type: INITIAL_LOAD_SUCCESS, payload: res.data})
+            })
+            .catch(err => {
+                console.log('inside action catch error for initialLoad')
+                dispatch({type: INITIAL_LOAD_FAILURE, payload: err})
+            })
+
+}
+
+export const loginDeveloper = (credentials) => {
+    console.log("being login action")
+    return (dispatch) =>{
+        dispatch({ type: LOGIN_DEVELOPER_START})
+        return axiosWithAuth()
+            .post(apiURL_Login_Developer, credentials)
+            .then(res=>{
+                console.log("Inside axios with auth in login Developer action")
+                dispatch({ type: LOGIN_DEVELOPER_SUCCESS, payload: res.data})
+            })
+            .catch(err=>{
+                dispatch({ type: LOGIN_DEVELOPER_FAILURE})
+            })
+    }
+}
+
